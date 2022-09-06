60e1dd19282d4f8c40793011278d09a9280d0e0f
Bladeyy <getsuga369@gmail.com>
2022-09-06 09:55:00 +0530

    sm8250-common: Enable USB debugging at boot

--- kona.mk
+++ kona.mk
@@ -447,6 +447,13 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.2-service-xiaomi_kona
 
+# USB debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.adb.secure=0 \
+    ro.secure=0 \
+    ro.debuggable=1
+
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
